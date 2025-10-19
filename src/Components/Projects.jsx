6127b94c5e31b955d602b04330a8ba3b977ed989
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-10-19 21:25:12 +0200

    add Projects component with project data and ProjectCardintergration.

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,16 @@
+import ProjectCard from './ProjectCard';
+
+const Projects = () => {
+  const projects = [
+    {
+      title: 'Yebopets Booking  Platform',
+      description: 'A platform that allows young dog owners to book trusted and verified pet sitters for their furry friends with ease and confidence.',
+      link: 'https://github.com/example/ecommerce'
+    },
+    {
+      title: 'Website design for a local business',
+      description: 'A modern and responsive website designed for a local businesses to showcase their products and attract more customers.',
+      link: 'https://github.com/example/taskmanager'
+    }
+  ];
+};
